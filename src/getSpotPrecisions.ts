2ace5ba80f717f30ca2c995e2b06c43517a53685
Ihor Malaniuk <ihor.malaniuk@gmail.com>
2024-08-05 20:50:38 +0300

    remove lodash fromEntries

--- src/getSpotPrecisions.ts
+++ src/getSpotPrecisions.ts
@@ -1,5 +1,4 @@
 import Decimal from 'decimal.js';
-import * as lo from 'lodash';
 
 import { parseList, rest } from './internal';
 
@@ -7,7 +6,7 @@ export const getSpotPrecisions = async () => {
   const query = rest.getInstrumentsInfo({ category: 'spot' });
   const list = await parseList('get-instruments-bybit', query);
 
-  return lo.fromPairs(
+  return Object.fromEntries(
     list.map((item) => {
       const price = new Decimal(item.priceFilter.tickSize);
       const qty = new Decimal(item.lotSizeFilter.basePrecision);
--- src/getSpotPrices.ts
+++ src/getSpotPrices.ts
@@ -1,10 +1,9 @@
 import Decimal from 'decimal.js';
-import * as lo from 'lodash';
 
 import { parseList, rest } from './internal';
 
 export const getSpotPrices = async (coin: string) =>
-  lo.fromPairs(
+  Object.fromEntries(
     (await parseList('get-prices-bybit', rest.getTickers({ category: 'spot' })))
       .filter((item) => item.symbol.endsWith(coin))
       .map((item) => [item.symbol, new Decimal(item.lastPrice)]),
