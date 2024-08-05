b54556ccc5df456ab261813cae1f953485c781a0
Ihor Malaniuk <ihor.malaniuk@gmail.com>
2024-08-05 23:10:14 +0300

    ws skip empty price, parse ticker price

--- src/wsUpdates.ts
+++ src/wsUpdates.ts
@@ -1,3 +1,19 @@
+import Decimal from 'decimal.js';
+
 import { socket } from './internal';
 
-export const wsUpdates = (cb: (data: any) => void) => socket.on('update', cb);
+export const wsUpdates = (cb: (data: any) => void) =>
+  socket.on('update', ({ topic, data }) => {
+    if (topic.startsWith('tickers.')) {
+      if (!data.lastPrice) {
+        return;
+      }
+
+      const price = new Decimal(data.lastPrice);
+      cb({ topic, data: { price } });
+
+      return;
+    }
+
+    cb({ topic, data });
+  });
