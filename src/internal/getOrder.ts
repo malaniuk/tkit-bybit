1b4301352ca31de5fe29c807f5523938333cc24c
Ihor Malaniuk <ihor.malaniuk@gmail.com>
2024-08-18 00:41:56 +0300

    getOrder optional symbol for linears

--- src/internal/getOrder.ts
+++ src/internal/getOrder.ts
@@ -2,8 +2,8 @@ import { category } from './config';
 import { parseList } from './parseList';
 import { rest } from './rest';
 
-export const getOrder = async (orderId: string) => {
-  const query = rest.getActiveOrders({ category, orderId });
+export const getOrder = async (orderId: string, symbol?: string) => {
+  const query = rest.getActiveOrders({ category, orderId, symbol });
   const list = await parseList('get-order-bybit', query);
 
   return list[0];
