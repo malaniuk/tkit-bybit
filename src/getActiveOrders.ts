29d148fe032c09db0a041906c8c7285c9522926f
Ihor Malaniuk <ihor.malaniuk@gmail.com>
2024-08-18 22:47:37 +0300

    getActiveOrders use settleCoin instead of symbol

--- src/getActiveOrders.ts
+++ src/getActiveOrders.ts
@@ -1,10 +1,10 @@
 import { category, parseList, rest } from './internal';
 
-export const getActiveOrders = async (symbol?: string) =>
+export const getActiveOrders = async (settleCoin?: string) =>
   parseList(
     'get-order-bybit',
     rest.getActiveOrders({
       category,
-      symbol,
+      settleCoin,
     }),
   );
