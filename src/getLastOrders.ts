f992b0309a7d6f046409f9b2b58cd5f80c3b9805
Ihor Malaniuk <ihor.malaniuk@gmail.com>
2024-10-12 21:53:34 +0300

    getLastOrders openOnly=2

--- src/getLastOrders.ts
+++ src/getLastOrders.ts
@@ -1,7 +1,7 @@
 import { category, rest } from './internal';
 
 export const getLastOrders = async (symbol?: string) => {
-  const resp = await rest.getActiveOrders({ category, symbol, openOnly: 1 });
+  const resp = await rest.getActiveOrders({ category, symbol, openOnly: 2 });
   if (resp.retCode !== 0) {
     throw { key: 'get-order-bybit', obj: resp };
   }
