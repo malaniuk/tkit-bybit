0908161e00dd89bb627cf3f609257337006ad1a3
Ihor Malaniuk <ihor.malaniuk@gmail.com>
2024-10-12 21:32:12 +0300

    getLastOrders not only active

--- src/getLastOrders.ts
+++ src/getLastOrders.ts
@@ -1,7 +1,7 @@
 import { category, rest } from './internal';
 
 export const getLastOrders = async (symbol?: string) => {
-  const resp = await rest.getActiveOrders({ category, symbol });
+  const resp = await rest.getActiveOrders({ category, symbol, openOnly: 1 });
   if (resp.retCode !== 0) {
     throw { key: 'get-order-bybit', obj: resp };
   }
