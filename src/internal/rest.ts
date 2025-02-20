a71bcbe6f7907cb36b066e5099a797ca6a8ded65
Ihor Malaniuk <ihor.malaniuk@gmail.com>
2025-02-20 22:56:58 +0200

    allow testnet

--- src/internal/rest.ts
+++ src/internal/rest.ts
@@ -4,4 +4,9 @@ const key = process.env.BB_API_KEY;
 const secret = process.env.BB_API_SECRET;
 const testnet = process.env.BB_TESTNET === 'true';
 
-export const rest = new RestClientV5({ key, secret, testnet });
+export const rest = new RestClientV5({
+  key,
+  secret,
+  testnet,
+  demoTrading: testnet,
+});
